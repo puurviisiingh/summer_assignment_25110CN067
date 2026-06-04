a643e4ca65b8dceba107aa9315bd886b2f029378
Purvi Singh <puurviisiingh@gmail.com>
2026-06-04 17:56:06 +0530

    Create products of digits.java

---
+++ day_2/products of digits.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class prodofdig {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int r,s=1,n;
+        System.out.println("Enter a number");
+        n=sc.nextInt();
+        while(n>0)
+        {
+            r=n%10;
+            n=n/10;
+            s=s*r;
+        }
+        System.out.println("sum of digit="+s);
+    }
+    
+}
