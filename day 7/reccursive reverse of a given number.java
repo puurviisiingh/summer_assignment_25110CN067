a868e01bf1000e4946ad757bc7cc6e48e2ff6dca
Purvi Singh <puurviisiingh@gmail.com>
2026-06-29 00:15:56 +0530

    Update reccursive reverse of a given number.java

--- day 7/reccursive reverse of a given number.java
+++ day 7/reccursive reverse of a given number.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class recsumofdigits {
+public class recreverse {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter a number");
