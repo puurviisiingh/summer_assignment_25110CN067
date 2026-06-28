7617ce0c9ef2b5644750424d8d250ace551e4b32
Purvi Singh <puurviisiingh@gmail.com>
2026-06-29 00:10:53 +0530

    Create reccursive reverse of a given number.java

---
+++ day 7/reccursive reverse of a given number.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class recsumofdigits {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a number");
+        int n=sc.nextInt();
+        int a=reverse(n,0);
+        System.out.println("reccursive sum of digits of a number="+a);
+        sc.close();
+    }
+    static int reverse(int n ,int s)
+    {
+        if(n==0)
+        {
+            return s;
+        }
+        else
+        {
+            s=(s*10)+(n%10);
+            return reverse(n/10, s);
+        }
+    }
+}
